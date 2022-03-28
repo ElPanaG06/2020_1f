894f606d233fe4df5244f9ea45f511458b442ae5
ElPanaG06 <gonzalogiles06@gmail.com>
2022-03-28 02:09:44 -0300

    Start CLASE 2

---
+++ clase_2/src/clase_2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : clase_2.c
+ Author      : gonzalogiles
+ Version     : 2.0 pa
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
